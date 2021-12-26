e0c9d3cc50bccca7435a23b9b8d4f457c4ace470
Pavel Grigorenko <311639@niuitmo.ru>
2021-12-26 17:40:02 +0300

    is_pe and command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use pe_parser::{err_to_string, is_pe, read_all};
+use std::{env, fs::File, io, process::exit};
+
+enum Mode {
+    IsPe { filepath: Option<String> },
+}
+
+fn parse_args(mut args: impl Iterator<Item = String>) -> Result<Mode, String> {
+    use Mode::*;
+    match args.nth(1) {
+        Some(mode) => {
+            match &*mode {
+                "is-pe" => Ok(IsPe {
+                    filepath: args.next(),
+                }),
+                _ => Err("Unknown mode: usage".to_string()), //todo
+            }
+        }
+        None => Err("No mode: usage".to_string()), //todo
+    }
+}
+
+fn run() -> Result<(), String> {
+    match parse_args(env::args())? {
+        Mode::IsPe { filepath } => {
+            let data = if let Some(filepath) = filepath {
+                read_all(File::open(filepath).map_err(err_to_string)?)?
+            } else {
+                read_all(io::stdin())?
+            };
+            if is_pe(&data) {
+                println!("PE");
+                exit(0);
+            } else {
+                println!("Not PE");
+                exit(1);
+            }
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        exit(-1);
+    }
 }

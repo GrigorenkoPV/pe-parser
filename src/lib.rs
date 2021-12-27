9a5d81aa437b87e818ee5bd5676b6e2205d18f62
Pavel Grigorenko <311639@niuitmo.ru>
2021-12-27 21:36:12 +0300

    import-functions: function names

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::fmt::Debug;
 use std::io;
 
 pub type Res<T> = Result<T, String>;
@@ -5,7 +6,8 @@ pub type Res<T> = Result<T, String>;
 const COFF_HEADER_SIZE: usize = 0x18;
 const OPTIONAL_HEADER_SIZE: usize = 0xF0;
 const SECTION_HEADER_SIZE: usize = 40;
-const IMPORT_TABLE_ENTRY_SIZE: usize = 20;
+const IDT_ENTRY_SIZE: usize = 20;
+const ILT_ENTRY_SIZE: usize = 8;
 
 pub fn err_to_string(err: impl std::fmt::Display) -> String {
     format!("{}", err)
@@ -29,6 +31,9 @@ fn get_u16(slice: &[u8], offset: usize) -> Option<u16> {
 }
 fn get_u32(slice: &[u8], offset: usize) -> Option<u32> {
     get_fixed_subslice::<4>(slice, offset).map(|slice| u32::from_le_bytes(*slice))
+}
+fn get_u64(slice: &[u8], offset: usize) -> Option<u64> {
+    get_fixed_subslice::<8>(slice, offset).map(|slice| u64::from_le_bytes(*slice))
 }
 
 fn strip_pe(data: &[u8]) -> Option<&[u8]> {
@@ -150,7 +155,7 @@ fn rva_to_raw(section_headers: &[SectionHeader], rva: u32) -> Res<usize> {
         })
 }
 
-fn terminate_by_null(data: &[u8], start_offset: usize) -> Res<String> {
+fn read_null_terminated_string(data: &[u8], start_offset: usize) -> Res<String> {
     let mut result = vec![];
     let mut current = start_offset;
     while let Some(&byte) = data.get(current) {
@@ -170,6 +175,72 @@ fn terminate_by_null(data: &[u8], start_offset: usize) -> Res<String> {
     ))
 }
 
+fn parse_ilt(file: &[u8], section_headers: &[SectionHeader], ilt_raw: usize) -> Res<Vec<String>> {
+    let mut result = vec![];
+    let mut ilt_entry_number = 0;
+    while let Some(ilt_entry) = get_u64(file, ilt_raw + ilt_entry_number * ILT_ENTRY_SIZE) {
+        if ilt_entry == 0 {
+            return Ok(result);
+        }
+        if ilt_entry & (1 << 63) == 0 {
+            let function_name_rva = ilt_entry as u32;
+            result.push(read_null_terminated_string(
+                file,
+                rva_to_raw(section_headers, function_name_rva)? + 2,
+            )?);
+        }
+        ilt_entry_number += 1;
+    }
+    Err(format!(
+        "File abruptly ended at 0x{:x} bytes before it was possible \
+        to read import lookup table entry #{} at 0x{:08x}",
+        file.len(),
+        ilt_entry_number,
+        ilt_raw + ilt_entry_number * ILT_ENTRY_SIZE
+    ))
+}
+
+fn parse_idt(
+    file: &[u8],
+    section_headers: &[SectionHeader],
+    idt_raw: usize,
+    idt_size: usize,
+) -> Res<Vec<(String, Vec<String>)>> {
+    let mut result = vec![];
+    let mut idt_entry_number = 0;
+    loop {
+        if (idt_entry_number + 1) * IDT_ENTRY_SIZE > idt_size {
+            return Err(format!(
+                "Reading entry #{} from the import directory table \
+                would exceed the size of the table ({} bytes)",
+                idt_entry_number, idt_size
+            ));
+        }
+        let idt_entry =
+            get_fixed_subslice::<IDT_ENTRY_SIZE>(file, idt_raw + idt_entry_number * IDT_ENTRY_SIZE)
+                .ok_or_else(|| {
+                    format!(
+                        "File abruptly ended at 0x{:x} bytes before \
+                        it was possible to read import directory table entry #{} at 0x{:08x}",
+                        file.len(),
+                        idt_entry_number,
+                        idt_raw + idt_entry_number * IDT_ENTRY_SIZE
+                    )
+                })?;
+        if idt_entry == &[0u8; IDT_ENTRY_SIZE] {
+            return Ok(result);
+        }
+        let ilt_rva = get_u32(idt_entry, 0x00).unwrap();
+        let dll_name_rva = get_u32(idt_entry, 0x0c).unwrap();
+        let dll_name_raw = rva_to_raw(section_headers, dll_name_rva)?;
+        result.push((
+            read_null_terminated_string(file, dll_name_raw)?,
+            parse_ilt(file, section_headers, rva_to_raw(section_headers, ilt_rva)?)?,
+        ));
+        idt_entry_number += 1;
+    }
+}
+
 pub fn import_functions(file: &[u8]) -> Res<Vec<(String, Vec<String>)>> {
     let pe = strip_pe(file).ok_or_else(|| "File too short to get its [0x3C].. part".to_string())?;
     let optional_header =
@@ -177,38 +248,10 @@ pub fn import_functions(file: &[u8]) -> Res<Vec<(String, Vec<String>)>> {
     let idt_rva = get_u32(optional_header, 0x78).unwrap();
     let idt_size = get_u32(optional_header, 0x7c).unwrap() as usize;
     let section_headers = get_section_headers(pe, true, get_u16(pe, 0x06).unwrap() as usize)?;
-    let idt_raw = rva_to_raw(&section_headers, idt_rva)?;
-    let mut result = vec![];
-    let mut idt_entry_number = 0;
-    loop {
-        if (idt_entry_number + 1) * IMPORT_TABLE_ENTRY_SIZE > idt_size {
-            return Err(format!(
-                "Reading entry #{} from the import table would exceed size of the table ({} bytes)",
-                idt_entry_number, idt_size
-            ));
-        }
-        let idt_entry = get_fixed_subslice::<IMPORT_TABLE_ENTRY_SIZE>(
-            file,
-            idt_raw + idt_entry_number * IMPORT_TABLE_ENTRY_SIZE,
-        )
-        .ok_or_else(|| {
-            format!(
-                "File abruptly ended at 0x{:x} bytes before \
-                it was possible to read import table entry #{} at 0x{:08x}",
-                file.len(),
-                idt_entry_number,
-                idt_size + idt_entry_number * IMPORT_TABLE_ENTRY_SIZE
-            )
-        })?;
-        if idt_entry == &[0u8; IMPORT_TABLE_ENTRY_SIZE] {
-            return Ok(result);
-        }
-        let dll_name_rva = get_u32(idt_entry, 0x0c).unwrap();
-        let dll_name_raw = rva_to_raw(&section_headers, dll_name_rva)?;
-        let dll_name = terminate_by_null(file, dll_name_raw)?;
-        let mut functions = vec![];
-        // todo
-        result.push((dll_name, functions));
-        idt_entry_number += 1;
-    }
+    parse_idt(
+        file,
+        &section_headers,
+        rva_to_raw(&section_headers, idt_rva)?,
+        idt_size,
+    )
 }
